19aa6e5a63ee1d9b06fd68a55657f288d2c121a2
Spencer <spencer@uws111580usr.uws.ualberta.ca>
2017-01-31 12:10:46 -0700

    farts

---
+++ Assets/Scripts/ToggleFartScore.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleFartScore : MonoBehaviour {
+
+	public GameObject fartSprite;
+	public GameObject fartScore;
+	public GameObject fartText;
+	public GameObject background;
+	public AudioClip fart1;
+	public AudioClip fart2;
+
+	private int delay;
+	private float startTime;
+	private float endTime;
+	private bool waiting;
+	private float fartShowTimeStart;
+	private bool fartShowing;
+
+	// Use this for initialization
+	void Start () {
+		waiting = false;
+		startTime = Time.time;
+		hideFart ();
+		fartShowing = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!waiting) {
+			delay = Random.Range (3, 8);
+			endTime = startTime + delay;
+			waiting = true;
+
+		} else { // if we are waiting, compare time
+			if (Time.time >= endTime) {
+				// show the score
+				showFart ();
+				fartShowing = true;
+
+				playFartSound ();
+
+				fartShowTimeStart = Time.time;
+				startTime = endTime + 1; // account for 1 second of fart showing
+				waiting = false;
+			}
+
+			if (fartShowing) {
+				if (Time.time >= fartShowTimeStart + 1) {
+					hideFart ();
+					fartShowing = false;
+				}
+			}
+		
+		}
+		
+	}
+
+	void hideFart() {
+		fartSprite.GetComponent<SpriteRenderer> ().enabled = false;
+		fartScore.GetComponent<Text> ().enabled = false;
+		fartText.GetComponent<Text> ().enabled = false;
+	}
+		
+	void showFart() {
+		fartSprite.GetComponent<SpriteRenderer> ().enabled = true;
+		fartScore.GetComponent<Text> ().enabled = true;
+		fartText.GetComponent<Text> ().enabled = true;
+	}
+
+	void playFartSound() {
+		int whichToPlay = Random.Range (1, 3);
+		AudioSource source = background.GetComponent<AudioSource> ();
+		if (whichToPlay == 1) {
+			source.clip = fart1;
+		} else {
+			source.clip = fart2;
+		}
+		source.Play ();
+	}
+}
