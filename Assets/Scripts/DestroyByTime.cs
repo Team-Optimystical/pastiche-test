c400a1d69430a05bda28171e0db901b43dd3ef17
Ryan <kobitoko@gmail.com>
2017-01-31 13:31:09 -0700

    Made explosion explody pew pew.

--- Assets/Scripts/DestroyByTime.cs
+++ Assets/Scripts/DestroyByTime.cs
@@ -8,4 +8,11 @@ public class DestroyByTime : MonoBehaviour
   {
     Destroy(gameObject, lifetime);
   }
+	void FixedUpdate()
+	{
+		Transform t = this.GetComponent<Transform> ();
+		SpriteRenderer s = this.GetComponent<SpriteRenderer> ();
+		t.localScale *= 1.05f;
+		s.color -= new Color (0f,0f,0f,0.05f);
+	}	
 }
